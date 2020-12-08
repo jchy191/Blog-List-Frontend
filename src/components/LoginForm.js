35c7d84c2cf2da52fd7ada8a245cebd6882c30d4
Joshua Chin <joshuachin191@gmail.com>
2020-12-08 16:04:37 +0800

    initialised repo

---
+++ src/components/LoginForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+LoginForm.propTypes = {
+  
+};
+
+function LoginForm(props) {
+  return (
+    <div>
+      <label>Username: <input onChange={} value={} name='username' type='text'/></label>
+      <label>Password: <input onChange={} value={} name='username' type='password'/></label>
+    </div>
+  );
+}
+
+export default LoginForm;
